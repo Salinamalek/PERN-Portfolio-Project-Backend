49f813e7a4e903f55ae1dde60fadf87584d04df9
Salinamalek <salinamalek@pursuit.org>
2023-02-08 12:14:53 -0500

    app.js done, backend done atm

---
+++ app.js
@@ -0,0 +1,30 @@
+// DEPENDENCIES
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const bucketlistController = require("./controllers/bucketlistController.js");
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cors());
+app.use("/list", bucketlistController);
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.status(200).send("Bucket List Server");
+});
+
+app.get("/not-found", (req, res) => {
+  res.status(404).json({ error: "page not found" });
+});
+
+app.get("*", (req, res) => {
+  res.redirect("/not-found");
+});
+
+// EXPORT
+module.exports = app;
