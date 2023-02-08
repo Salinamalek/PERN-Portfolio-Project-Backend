abada21f0ffc539b7235bfa21733c49bd5b45c7e
Salinamalek <salinamalek@pursuit.org>
2023-02-08 12:07:50 -0500

    controller done

---
+++ controllers/bucketlistController.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const lists = express.Router();
+
+const {
+  getAllList,
+  getOneList,
+  createList,
+  updateList,
+  deleteList,
+} = require("../queries/bucketlist.js");
+
+// GET ALL
+lists.get("/", async (req, res) => {
+  const allLists = await getAllList();
+
+  if (allLists.length) {
+    req.status(200).json(allLists);
+  } else {
+    res.status(500).json({ Error: "server error" });
+  }
+});
+
+// GET ONE
+lists.get(":id", async (req, res) => {
+  const { id } = req.params;
+  const list = await getOneList(id);
+
+  if (!list.message) {
+    res.status(200).json(list);
+  } else {
+    req.redirect("/not-found");
+  }
+});
+
+// CREATE
+lists.post("/", async (req, res) => {
+  const newList = await createList(req.body);
+  if (!newList.message) {
+    res.status(200).json(newList);
+  } else {
+    res.status(500).json({ error: newList.message });
+  }
+});
+
+// UPDATE
+lists.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const updatedList = await updateList(req.body, id);
+  if (!updatedList.message) {
+    res.status(200).json(updatedList);
+  } else {
+    res.status(500).json({ error: updatedList.message });
+  }
+});
+
+// DELETE
+lists.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedList = await deleteList(id);
+
+  if (!deletedList.message) {
+    res.status(200).json(deletedList);
+  } else {
+    res.status(500).json({ error: deletedList.message });
+  }
+});
+
+module.exports = lists;
